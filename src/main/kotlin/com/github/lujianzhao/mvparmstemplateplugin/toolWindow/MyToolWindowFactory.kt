8bd92c1c48436fec7aea12390bf1d4d7552ac055
GitHub Action <action@github.com>
2023-04-18 01:09:12 +0000

    Template cleanup

--- src/main/kotlin/com/github/lujianzhao/mvparmstemplateplugin/toolWindow/MyToolWindowFactory.kt
+++ src/main/kotlin/com/github/lujianzhao/mvparmstemplateplugin/toolWindow/MyToolWindowFactory.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.toolWindow
+package com.github.lujianzhao.mvparmstemplateplugin.toolWindow
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.diagnostic.thisLogger
@@ -8,8 +8,8 @@ import com.intellij.openapi.wm.ToolWindowFactory
 import com.intellij.ui.components.JBLabel
 import com.intellij.ui.components.JBPanel
 import com.intellij.ui.content.ContentFactory
-import org.jetbrains.plugins.template.MyBundle
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.lujianzhao.mvparmstemplateplugin.MyBundle
+import com.github.lujianzhao.mvparmstemplateplugin.services.MyProjectService
 import javax.swing.JButton
 
 
